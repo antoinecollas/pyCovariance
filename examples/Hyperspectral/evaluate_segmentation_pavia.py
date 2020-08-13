665050470520d99de8fba405a1b00fe0d212c015
Antoine Collas <contact@antoinecollas.fr>
2020-08-13 14:56:41 +0200

    Cleaner evaluation

--- examples/Hyperspectral/evaluate_segmentation_pavia.py
+++ examples/Hyperspectral/evaluate_segmentation_pavia.py
@@ -26,10 +26,9 @@ from clustering_SAR.generic_functions import assign_classes_segmentation_to_gt, 
 # segmentation path (by default: get last folder)
 FOLDER_REGEX = 'results/*'
 folder_result = glob.glob(FOLDER_REGEX)[0]
-segmentation = glob.glob(folder_result+'/*.npy')[0]
-print('Segmentation file used :', segmentation)
-segmentation = np.load(segmentation)
-segmentation += 1
+print('Folder used to get segmentations:', folder_result)
+segmentations_paths = glob.glob(folder_result+'/*.npy')
+segmentations_paths.sort()
 
 # ground truth path
 PATH_GT = 'data/Pavia/PaviaU_gt.mat'
@@ -40,7 +39,9 @@ h = WINDOWS_SHAPE[0]//2
 w = WINDOWS_SHAPE[1]//2
 gt = gt[h:-h, w:-w]
 
-assert segmentation.shape == gt.shape
+for path in segmentations_paths:
+    segmentation = np.load(path)
+    assert segmentation.shape == gt.shape
 
 #######################################################
 #######################################################
@@ -48,37 +49,54 @@ assert segmentation.shape == gt.shape
 #######################################################
 #######################################################
 
-print()
-print('################################################')
-print('Supervised metric')
-print('################################################')
-old_segmentation = copy.deepcopy(segmentation)
-IoU, mIoU = compute_mIoU(old_segmentation, gt, np.unique(segmentation))
-print('mIoU before Hungarian algo=', mIoU)
-segmentation = assign_classes_segmentation_to_gt(segmentation, gt, normalize=True)
-IoU, mIoU = compute_mIoU(segmentation, gt, np.unique(segmentation))
-print('mIoU=', mIoU)
-classes = np.unique(segmentation).astype(np.int)
-for i, class_ in enumerate(classes):
-    print('class', class_, ': IoU=', round(IoU[i], 2))
+# create folder to save analyses
+folder = os.path.join(folder_result, 'analyses')
+if not os.path.isdir(folder):
+    os.mkdir(folder)
 
+for path in segmentations_paths:
+    segmentation = np.load(path)
+    name = path.split('/')[-1].split('.')[0]
+    folder_analyses = os.path.join(folder, name)
+    if not os.path.isdir(folder_analyses):
+        os.mkdir(folder_analyses)
 
-print()
-print('################################################')
-print('Unsupervised metric')
-print('################################################')
-true = gt[gt!=0]
-pred = segmentation[gt!=0]
-AMI = adjusted_mutual_info_score(true, pred)
-ARI = adjusted_rand_score(true, pred)
-print('AMI=', AMI)
-print('ARI=', ARI)
+    print('################################################')
+    print('################################################')
+    print()
+    print('Results from', name)
+    
+    print('################################################')
+    print('Supervised metric')
+    print('################################################')
+    old_segmentation = copy.deepcopy(segmentation)
+    IoU, mIoU = compute_mIoU(old_segmentation, gt, np.unique(segmentation))
+    print('mIoU before Hungarian algo=', mIoU)
+    segmentation = assign_classes_segmentation_to_gt(segmentation, gt, normalize=False)
+    IoU, mIoU = compute_mIoU(segmentation, gt, np.unique(segmentation))
+    print('mIoU=', mIoU)
+    classes = np.unique(segmentation).astype(np.int)
+    temp = ''
+    for i, class_ in enumerate(classes):
+        temp += 'class '+  str(class_) + ': IoU=' + str(round(IoU[i], 2)) + ' '
+    print(temp)
 
-print()
-print('################################################')
-print('Plot')
-print('################################################')
-plot_segmentation(segmentation, classes=np.unique(gt).astype(np.int), title='Segmentation')
-plot_segmentation(gt, title='Ground truth')
-plot_TP_FP_FN_segmentation(segmentation, gt)
-plt.show()
+    print('################################################')
+    print('Unsupervised metric')
+    print('################################################')
+    true = gt[gt!=0]
+    pred = segmentation[gt!=0]
+    AMI = adjusted_mutual_info_score(true, pred)
+    ARI = adjusted_rand_score(true, pred)
+    print('AMI=', AMI)
+    print('ARI=', ARI)
+    print('################################################')
+    print('Plots saved in', folder_analyses)
+
+    plot_segmentation(segmentation, classes=np.unique(gt).astype(np.int), title='Segmentation')
+    plt.savefig(os.path.join(folder_analyses, 'segmentation.png'))
+    plot_segmentation(gt, title='Ground truth')
+    plt.savefig(os.path.join(folder_analyses, 'gt.png'))
+    plot_TP_FP_FN_segmentation(segmentation, gt, folder_save=folder_analyses)
+    
+    plt.close('all')
