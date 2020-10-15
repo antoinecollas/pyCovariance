1d3e908560e6f033074abd1c9564f2204a1f79dd
Antoine Collas <contact@antoinecollas.fr>
2020-10-15 11:09:19 +0200

    remove tyler with location pre estimated

--- examples/numerical_simulations/estimation_location_cov_texture/error_estimation.py
+++ examples/numerical_simulations/estimation_location_cov_texture/error_estimation.py
@@ -36,20 +36,15 @@ mu_errors_g = list()
 tau_errors_g = list()
 sigma_errors_g = list()
 
-# Tyler with mu estimated
+# Tyler location known
 mu_errors_t = list()
 tau_errors_t = list()
 sigma_errors_t = list()
 
-# Tyler location known
+# Tyler
 mu_errors_t2 = list()
 tau_errors_t2 = list()
 sigma_errors_t2 = list()
-
-# Tyler
-mu_errors_t3 = list()
-tau_errors_t3 = list()
-sigma_errors_t3 = list()
 
 # Riemannian gradient descent
 mu_errors_rgd = list()
@@ -62,20 +57,15 @@ for n in tqdm(list_n_points):
     tau_error_g = list()
     sigma_error_g = list()
 
-    # Tyler
+    # Tyler with mu known
     mu_error_t = list()
     tau_error_t = list()
     sigma_error_t = list()
 
-    # Tyler with mu known
+    # Tyler
     mu_error_t2 = list()
     tau_error_t2 = list()
     sigma_error_t2 = list()
-
-    # Tyler
-    mu_error_t3 = list()
-    tau_error_t3 = list()
-    sigma_error_t3 = list()
 
     # Riemannian gradient descent
     mu_error_rgd = list()
@@ -100,19 +90,6 @@ for n in tqdm(list_n_points):
         # Initialisation
         theta_0 = [mu_g, tau_g, sigma_g]
 
-        # Tyler location pre-estimated
-        X_centered = X-mu_g
-        tau_est, sigma_est, _, _ = tyler_estimator_covariance_normalisedet(
-            X_centered,
-            init=[theta_0[1], theta_0[2]],
-            tol=tol,
-            iter_max=iter_max,
-        )
-        mu_est = theta_0[0]
-        mu_error_t.append(np.linalg.norm(mu-mu_est)**2)
-        tau_error_t.append((distance_texture_Riemannian(tau, tau_est)**2)/n)
-        sigma_error_t.append(distance_covariance_Riemannian(vech(sigma_est), vech(sigma))**2)
-
         # Tyler location known
         X_centered = X-mu
         X_centered_filtered = X_centered[:, np.linalg.norm(X_centered, axis=0)>1e-8]
@@ -124,9 +101,9 @@ for n in tqdm(list_n_points):
         )
         mu_est = mu
         tau_est = (1/p) * np.real(np.einsum('ij,ji->i', np.conjugate(X_centered).T@np.linalg.inv(sigma_est), X_centered))
-        mu_error_t2.append(np.linalg.norm(mu-mu_est)**2)
-        tau_error_t2.append((distance_texture_Riemannian(tau, tau_est)**2)/n)
-        sigma_error_t2.append(distance_covariance_Riemannian(vech(sigma_est), vech(sigma))**2)
+        mu_error_t.append(np.linalg.norm(mu-mu_est)**2)
+        tau_error_t.append((distance_texture_Riemannian(tau, tau_est)**2)/n)
+        sigma_error_t.append(distance_covariance_Riemannian(vech(sigma_est), vech(sigma))**2)
 
         # Tyler
         mu_est, tau_est, sigma_est, _, _ = tyler_estimator_location_covariance_normalisedet(
@@ -135,16 +112,16 @@ for n in tqdm(list_n_points):
             tol=tol,
             iter_max=iter_max,
         )
-        mu_error_t3.append(np.linalg.norm(mu-mu_est)**2)
-        tau_error_t3.append((distance_texture_Riemannian(tau, tau_est)**2)/n)
-        sigma_error_t3.append(distance_covariance_Riemannian(vech(sigma_est), vech(sigma))**2)
+        mu_error_t2.append(np.linalg.norm(mu-mu_est)**2)
+        tau_error_t2.append((distance_texture_Riemannian(tau, tau_est)**2)/n)
+        sigma_error_t2.append(distance_covariance_Riemannian(vech(sigma_est), vech(sigma))**2)
 
         # gradient descent 
         mu_est, tau_est, sigma_est, _ = estimation_location_covariance_texture_RGD(
             X,
             init=theta_0,
             tol=tol,
-            iter_max=iter_max,
+            iter_max=1,
             autodiff=False
         )
         mu_error_rgd.append(np.linalg.norm(mu-mu_est)**2)
@@ -156,20 +133,15 @@ for n in tqdm(list_n_points):
     tau_errors_g.append(np.mean(tau_error_g))
     sigma_errors_g.append(np.mean(sigma_error_g))
 
-    # Tyler location pre-estimated
+    # Tyler location known
     mu_errors_t.append(np.mean(mu_error_t))
     tau_errors_t.append(np.mean(tau_error_t))
     sigma_errors_t.append(np.mean(sigma_error_t))
 
-    # Tyler location known
+    # Tyler
     mu_errors_t2.append(np.mean(mu_error_t2))
     tau_errors_t2.append(np.mean(tau_error_t2))
     sigma_errors_t2.append(np.mean(sigma_error_t2))
-
-    # Tyler
-    mu_errors_t3.append(np.mean(mu_error_t3))
-    tau_errors_t3.append(np.mean(tau_error_t3))
-    sigma_errors_t3.append(np.mean(sigma_error_t3))
 
     # gradient descent 
     mu_errors_rgd.append(np.mean(mu_error_rgd))
@@ -179,7 +151,7 @@ for n in tqdm(list_n_points):
 path = 'results/location_tyler_vs_grad'
 
 plt.loglog(list_n_points, mu_errors_g, marker='*', color='b', label='mu - Gaussian')
-plt.loglog(list_n_points, mu_errors_t3, marker='+', color='g', label='mu - Tyler')
+plt.loglog(list_n_points, mu_errors_t2, marker='+', color='g', label='mu - Tyler')
 plt.loglog(list_n_points, mu_errors_rgd, marker='.', color='r', label='mu - Riemannian')
 plt.legend()
 plt.xlabel('Number of data points')
@@ -190,9 +162,8 @@ tikzplotlib.save(path+'_mu.tex')
 plt.clf()
 
 plt.loglog(list_n_points, tau_errors_g, marker='*', color='b', label='tau - Gaussian')
-plt.loglog(list_n_points, tau_errors_t, marker='s', color='c', label='tau - Tyler - location pre-estimated')
-plt.loglog(list_n_points, tau_errors_t2, marker='^', color='k', label='tau - Tyler - location known')
-plt.loglog(list_n_points, tau_errors_t3, marker='+', color='g', label='tau - Tyler')
+plt.loglog(list_n_points, tau_errors_t, marker='^', color='k', label='tau - Tyler - location known')
+plt.loglog(list_n_points, tau_errors_t2, marker='+', color='g', label='tau - Tyler')
 plt.loglog(list_n_points, tau_errors_rgd, marker='.', color='r', label='tau - Riemannian')
 plt.legend()
 plt.xlabel('Number of data points')
@@ -203,9 +174,8 @@ tikzplotlib.save(path+'_tau.tex')
 plt.clf()
 
 plt.loglog(list_n_points, sigma_errors_g, marker='*', color='b', label='sigma - Gaussian')
-plt.loglog(list_n_points, sigma_errors_t, marker='s', color='c', label='sigma - Tyler - location pre-estimated')
-plt.loglog(list_n_points, sigma_errors_t2, marker='^', color='k', label='sigma - Tyler - location known')
-plt.loglog(list_n_points, sigma_errors_t3, marker='+', color='g', label='sigma - Tyler')
+plt.loglog(list_n_points, sigma_errors_t, marker='^', color='k', label='sigma - Tyler - location known')
+plt.loglog(list_n_points, sigma_errors_t2, marker='+', color='g', label='sigma - Tyler')
 plt.loglog(list_n_points, sigma_errors_rgd, marker='.', color='r', label='sigma - Riemannian')
 plt.legend()
 plt.xlabel('Number of data points')
