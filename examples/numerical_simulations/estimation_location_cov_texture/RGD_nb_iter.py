a3f9ec700f36a4cc652e6c57f02b047306425280
Antoine Collas <contact@antoinecollas.fr>
2020-10-20 15:24:25 +0200

    change plot

--- examples/numerical_simulations/estimation_location_cov_texture/RGD_nb_iter.py
+++ examples/numerical_simulations/estimation_location_cov_texture/RGD_nb_iter.py
@@ -47,11 +47,9 @@ for i in tqdm(range(nb_MC)):
     f_values_conjugate.append(np.array(log['iterations']['f(x)']))
     grad_norm_values_conjugate.append(log['iterations']['gradnorm'])
 
-min_f_values = np.min(np.concatenate([np.array(f_values_steepest), np.array(f_values_conjugate)]))
 
 f_values_steepest = np.stack(f_values_steepest)
 f_values_steepest = np.mean(f_values_steepest, axis=0)
-f_values_steepest = f_values_steepest - min_f_values + 1
 
 grad_norm_values_steepest = np.stack(grad_norm_values_steepest)
 grad_norm_values_steepest = grad_norm_values_steepest.reshape((grad_norm_values_steepest.shape[0], grad_norm_values_steepest.shape[1]))
@@ -59,7 +57,6 @@ grad_norm_values_steepest = np.mean(grad_norm_values_steepest, axis=0)
 
 f_values_conjugate = np.stack(f_values_conjugate)
 f_values_conjugate = np.mean(f_values_conjugate, axis=0)
-f_values_conjugate = f_values_conjugate - min_f_values + 1
 
 grad_norm_values_conjugate = np.stack(grad_norm_values_conjugate)
 grad_norm_values_conjugate = grad_norm_values_conjugate.reshape((grad_norm_values_conjugate.shape[0], grad_norm_values_conjugate.shape[1]))
@@ -68,8 +65,8 @@ grad_norm_values_conjugate = np.mean(grad_norm_values_conjugate, axis=0)
 
 list_iter = list(range(1, iter_max+1))
 
-plt.loglog(list_iter, f_values_steepest, label='steepest descent')
-plt.loglog(list_iter, f_values_conjugate, label='conjugate gradient')
+plt.semilogx(list_iter, f_values_steepest, label='steepest descent')
+plt.semilogx(list_iter, f_values_conjugate, label='conjugate gradient')
 plt.legend()
 plt.xlabel('Number of iterations')
 plt.ylabel('Negative log likelihood')
