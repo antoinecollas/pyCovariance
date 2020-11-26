7996769ca76f48a6cdda46b7638b3e6105e6f324
Antoine Collas <contact@antoinecollas.fr>
2020-11-26 12:14:11 +0100

    Add tests of evaluation functions

--- pyCovariance/evaluation.py
+++ pyCovariance/evaluation.py
@@ -5,9 +5,20 @@ from scipy.optimize import linear_sum_assignment
 import tikzplotlib
 
 
-def assign_classes_segmentation_to_gt(C, gt, normalize=False):
-    """ A function that assigns the classes of the segmentation to the ground truth.
-        BE CAREFUL : class 0 is always ignored, both in C and gt.
+def _get_classes(C, gt):
+    classes_C = np.unique(C).astype(np.int)
+    classes_gt = np.unique(gt).astype(np.int)
+    classes = np.array(list(set().union(classes_C, classes_gt)))
+    classes = classes.astype(np.int64)
+    classes = classes[classes >= 0]
+    classes = np.sort(classes)
+    return classes
+
+
+def assign_segmentation_classes_to_gt_classes(C, gt, normalize=False):
+    """ A function that assigns the classes of the segmentation to
+        the classes of the ground truth.
+        BE CAREFUL : negative classes are always ignored, both in C and gt.
         Inputs:
             * C: segmented image.
             * gt: ground truth.
@@ -16,11 +27,7 @@ def assign_classes_segmentation_to_gt(C, gt, normalize=False):
             * segmented image with the right classes.
     """
     # get classes
-    classes_C = np.unique(C).astype(np.int)
-    classes_gt = np.unique(gt).astype(np.int)
-    classes = np.array(list(set().union(classes_C, classes_gt)), dtype=np.int)
-    if classes[0] == 0:
-        classes = classes[1:]
+    classes = _get_classes(C, gt)
     nb_classes = len(classes)
 
     cost_matrix = np.zeros((nb_classes, nb_classes))
@@ -34,15 +41,58 @@ def assign_classes_segmentation_to_gt(C, gt, normalize=False):
             if normalize:
                 cost /= nb_pixels
             cost_matrix[i, j] = cost
-    
+
     row_ind, col_ind = linear_sum_assignment(cost_matrix)
-    row_ind = classes[row_ind] 
-    col_ind = classes[col_ind] 
+    row_ind = classes[row_ind]
+    col_ind = classes[col_ind]
     new_C = np.zeros(C.shape)
     for i, j in zip(col_ind, row_ind):
-        new_C[C==i] = j
+        new_C[C == i] = j
 
     return new_C
+
+
+def compute_mIoU(C, gt):
+    """ A function that computes the mean of Intersection over Union between
+    a segmented image (c) and a ground truth (gt).
+    BE CAREFUL, negative values are considered as no annotation available.
+        Inputs:
+            * C: segmented image.
+            * gt: ground truth.
+            * classes: list of classes used to compute the mIOU
+        Ouputs:
+            * IoU, mIOU
+    """
+    # get classes
+    classes = _get_classes(C, gt)
+
+    IoU = list()
+    for i in classes:
+        inter = np.sum((C == i) & (gt == i))
+        union = np.sum(((C == i) | (gt == i)) & np.isin(gt, classes))
+        IoU.append(inter/union)
+    mIoU = np.mean(IoU)
+    return IoU, mIoU
+
+
+def compute_OA(C, gt):
+    """ A function that computes the Overall Accuracy (OA) between
+    a segmented image (c) and a ground truth (gt).
+    BE CAREFUL, negative values are considered as no annotation available.
+        Inputs:
+            * C: segmented image.
+            * gt: ground truth.
+            * classes: list of classes used to compute the mIOU
+        Ouputs:
+            * OA
+    """
+    # get classes
+    classes = _get_classes(C, gt)
+
+    mask = np.isin(gt, classes)
+    OA = np.sum(C[mask] == gt[mask]) / np.sum(mask)
+
+    return OA
 
 
 def plot_segmentation(C, aspect=1, classes=None, title=None):
@@ -56,18 +106,18 @@ def plot_segmentation(C, aspect=1, classes=None, title=None):
     if classes is not None:
         max_C, min_C = np.max(classes), np.min(classes)
     else:
-        max_C, min_C = np.max(C), np.min(C) 
+        max_C, min_C = np.max(C), np.min(C)
 
-    #get discrete colormap
+    # get discrete colormap
     cmap = plt.get_cmap('RdBu', max_C-min_C+1)
- 
+
     # set limits .5 outside true range
-    mat = plt.matshow(C, aspect=aspect, cmap=cmap, vmin=min_C-.5, vmax=max_C+.5)
+    mat = plt.matshow(C, aspect, cmap, vmin=min_C-.5, vmax=max_C+.5)
 
-    #tell the colorbar to tick at integers
-    cax = plt.colorbar(mat, ticks=np.arange(min_C,max_C+1))
+    # tell the colorbar to tick at integers
+    plt.colorbar(mat, ticks=np.arange(min_C, max_C+1))
 
-    #title
+    # title
     if title is not None:
         plt.title(title)
 
@@ -75,7 +125,8 @@ def plot_segmentation(C, aspect=1, classes=None, title=None):
 
 
 def save_segmentation(folder, filename, np_array):
-    """ A function that saves a numpy array in a folder. The array and the folder are passed as arguments.
+    """ A function that saves a numpy array in a folder.
+    The array and the folder are passed as arguments.
         Inputs:
             * folder: string.
             * filename: string.
@@ -89,45 +140,46 @@ def save_segmentation(folder, filename, np_array):
 
 
 def plot_TP_FP_FN_segmentation(C, gt, aspect=1, folder_save=None):
-    """ Plot True Positive, False Positive, False Negative for a segmetnation given a ground truth. BE CAREFUL: class 0 is ignored !
+    """ Plot True Positive, False Positive, False Negative
+    for a segmetnation given a ground truth.
+    BE CAREFUL: negative classes are ignored
         Inputs:
             * C: a (height, width) numpy array of integers (classes).
             * gt: a (height, width) numpy array of integers (classes).
             * aspect: aspect ratio of the image.
-            * folder_save: string representing the path of the folder where to save the plots. If not, plots are not saved.
+            * folder_save: string of the path of the folder to save the plots.
+            If not, plots are not saved.
     """
     # get classes
-    classes_C = np.unique(C).astype(np.int)
-    classes_gt = np.unique(gt).astype(np.int)
-    classes = list(set().union(classes_C, classes_gt))
-    if classes[0] == 0:
-        classes = classes[1:]
-    
+    classes = _get_classes(C, gt)
+
     # get discrete colormap
     cmap = plt.get_cmap('RdBu', 4)
-    
+
     for i in classes:
         to_plot = np.zeros(C.shape)
         # true positive
         mask = np.logical_and((C == i), (gt == i))
         to_plot[mask] = 3
- 
+
         # false positive
-        mask = np.logical_and(np.logical_and((C == i), (gt != i)), (gt != 0))
+        mask = np.logical_and(np.logical_and((C == i),
+                                             (gt != i)), np.isin(gt, classes))
         to_plot[mask] = 2
 
         # false negative
         mask = np.logical_and((C != i), (gt == i))
         to_plot[mask] = 1
- 
+
         # set limits .5 outside true range
-        mat = plt.matshow(to_plot, aspect=aspect, cmap=cmap, vmin=-0.5, vmax=3.5)
+        mat = plt.matshow(to_plot, aspect=aspect,
+                          cmap=cmap, vmin=-0.5, vmax=3.5)
 
         # tell the colorbar to tick at integers
         cax = plt.colorbar(mat)
         cax.set_ticks(np.arange(0, 4))
         cax.set_ticklabels(['Other', 'FN', 'FP', 'TP'])
- 
+
         # title
         plt.title('Class '+str(i))
 
@@ -139,59 +191,13 @@ def plot_TP_FP_FN_segmentation(C, gt, aspect=1, folder_save=None):
             plt.savefig(path)
 
 
-def compute_mIoU(C, gt):
-    """ A function that computes the mean of Intersection over Union between a segmented image (c) and a ground truth (gt). BE CAREFUL, 0 is considered as no annotation available.
-        Inputs:
-            * C: segmented image.
-            * gt: ground truth.
-            * classes: list of classes used to compute the mIOU
-        Ouputs:
-            * IoU, mIOU
-    """
-    # get classes
-    classes_C = np.unique(C).astype(np.int)
-    classes_gt = np.unique(gt).astype(np.int)
-    classes = list(set().union(classes_C, classes_gt))
-    if classes[0] == 0:
-        classes = classes[1:]
-
-    IoU = list()
-    for i in classes:
-        inter = np.sum((C==i) & (gt==i))
-        union = np.sum(((C==i)|(gt==i)) & (gt!=0))
-        IoU.append(inter/union)
-    mIoU = np.mean(IoU)
-    return IoU, mIoU
-
-def compute_OA(C, gt):
-    """ A function that computes the Overall Accuracy (OA) between a segmented image (c) and a ground truth (gt). BE CAREFUL, 0 is considered as no annotation available.
-        Inputs:
-            * C: segmented image.
-            * gt: ground truth.
-            * classes: list of classes used to compute the mIOU
-        Ouputs:
-            * OA
-    """
-    # get classes
-    classes_C = np.unique(C).astype(np.int)
-    classes_gt = np.unique(gt).astype(np.int)
-    classes = list(set().union(classes_C, classes_gt))
-    if classes[0] == 0:
-        classes = classes[1:]
-    
-    mask = (gt != 0)
-    OA = np.sum(C[mask]==gt[mask]) / np.sum(mask)
-
-    return OA
-
-
 def plot_Pauli_SAR(image, aspect=1):
     """ 1st dimension =HH, 2nd dimnension = HV, 3rd dimension=VV"""
-    R = np.abs(image[:,:,0] - image[:,:,2])
-    G = np.abs(image[:,:,1])
-    B = np.abs(image[:,:,0] + image[:,:,2])
+    R = np.abs(image[:, :, 0] - image[:, :, 2])
+    G = np.abs(image[:, :, 1])
+    B = np.abs(image[:, :, 0] + image[:, :, 2])
     fig = plt.figure()
-    RGB_image = np.stack([R,G,B], axis=2)
+    RGB_image = np.stack([R, G, B], axis=2)
     RGB_image = RGB_image - np.min(RGB_image)
     RGB_image[RGB_image > 1] = 1
     plt.imshow(RGB_image, aspect=aspect)
@@ -202,13 +208,13 @@ def plot_Pauli_SAR(image, aspect=1):
 def save_figure(folder, figname):
     """ A function that save the current figure in '.png' and in '.tex'.
         Inputs:
-            * folder: string corresponding to the folder's name where to save the actual figure.
-            * figname: string corresponding to the name of the figure to save.
+            * folder: string of the folder's name where to save actual figure.
+            * figname: string of the name of the figure to save.
     """
     if not os.path.exists(folder):
         os.makedirs(folder, exist_ok=True)
     path = os.path.join(folder, figname)
-    
+
     path_png = path + '.png'
     plt.savefig(path_png)
 
---
+++ tests/test_evaluation.py
@@ -0,0 +1,118 @@
+import autograd.numpy as np
+from autograd.numpy import random as rnd
+from copy import deepcopy
+from sklearn.metrics import f1_score
+
+from pyCovariance.evaluation import\
+        assign_segmentation_classes_to_gt_classes,\
+        compute_mIoU,\
+        compute_OA
+
+
+def test_assign_segmentation_classes_to_gt_classes_and_compute_mIoU():
+    N = int(1e3)
+
+    # generation of ground truth
+    gt = np.zeros(N)
+    for i in range(len(gt)):
+        a = rnd.random_sample()
+        if a < 0.1:
+            gt[i] = 0
+        elif a < 0.5:
+            gt[i] = 1
+        else:
+            gt[i] = 2
+
+    # permutation of classes
+    C = gt
+    classes = np.unique(gt)
+    while (C == gt).any():
+        temp = rnd.permutation(classes)
+        C = deepcopy(gt)
+        for i, j in zip(classes, temp):
+            C[gt == i] = j
+
+    assert f1_score(gt[gt >= 0], C[gt >= 0], average='macro') == 0
+    _, mIoU = compute_mIoU(C, gt)
+    assert mIoU == 0
+    OA = compute_OA(C, gt)
+    assert OA == 0
+
+    C_temp = assign_segmentation_classes_to_gt_classes(C, gt, normalize=True)
+    assert f1_score(gt[gt >= 0], C_temp[gt >= 0], average='macro') == 1
+    _, mIoU = compute_mIoU(C_temp, gt)
+    assert mIoU == 1
+    OA = compute_OA(C_temp, gt)
+    assert OA == 1
+
+    # we add wrong classifications
+    for i in range(len(C)):
+        a = rnd.random_sample()
+        if a < 0.05:
+            C[i] = rnd.choice([0, 1, 2])
+
+    assert f1_score(gt, C, average='macro') < 0.1
+    _, mIoU = compute_mIoU(C, gt)
+    assert mIoU < 0.1
+
+    C = assign_segmentation_classes_to_gt_classes(C, gt)
+    assert f1_score(gt, C, average='macro') > 0.85
+    _, mIoU = compute_mIoU(C, gt)
+    assert mIoU > 0.85
+    OA = compute_OA(C, gt)
+    assert OA > 0.85
+
+    # test with negative classes (these classes should be ignored!)
+    # generation of ground truth
+    gt = np.zeros(N)
+    for i in range(len(gt)):
+        a = rnd.random_sample()
+        if a < 0.8:
+            gt[i] = -3
+        elif a < 0.9:
+            gt[i] = 0
+        elif a < 0.95:
+            gt[i] = 2
+        else:
+            gt[i] = 3
+
+    # permutation of classes
+    C = gt
+    classes = np.unique(gt[gt >= 0])
+    while (C == gt)[gt >= 0].any():
+        temp = rnd.permutation(classes)
+        C = deepcopy(gt)
+        for i, j in zip(classes, temp):
+            C[gt == int(i)] = int(j)
+
+    assert f1_score(gt[gt >= 0], C[gt >= 0], average='macro') == 0
+    _, mIoU = compute_mIoU(C, gt)
+    assert mIoU == 0
+    OA = compute_OA(C, gt)
+    assert OA == 0
+
+    C_temp = assign_segmentation_classes_to_gt_classes(C, gt)
+    assert f1_score(gt[gt >= 0], C_temp[gt >= 0], average='macro') == 1
+    _, mIoU = compute_mIoU(C_temp, gt)
+    assert mIoU == 1
+    OA = compute_OA(C_temp, gt)
+    assert OA == 1
+
+    # we add wrong classifications
+    for i in range(len(C)):
+        a = rnd.random_sample()
+        if a < 0.05:
+            C[i] = rnd.choice([0, 2, 3])
+
+    assert f1_score(gt[gt >= 0], C[gt >= 0], average='macro') < 0.1
+    _, mIoU = compute_mIoU(C, gt)
+    assert mIoU < 0.1
+    OA = compute_OA(C, gt)
+    assert OA < 0.1
+
+    C = assign_segmentation_classes_to_gt_classes(C, gt)
+    assert f1_score(gt[gt >= 0], C[gt >= 0], average='macro') > 0.85
+    _, mIoU = compute_mIoU(C, gt)
+    assert mIoU > 0.85
+    OA = compute_OA(C, gt)
+    assert OA > 0.85
