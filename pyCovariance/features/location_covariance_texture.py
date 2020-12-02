fix tests location + covariance + texture

pyCovariance/features/location_covariance_texture.py
@@ -227,7 +227,7 @@ def estimation_location_covariance_texture_RGD(
     X,
     init=None,
     tol=1e-3,
-    iter_max=1000,
+    iter_max=int(1e3),
     autodiff=False,
     solver='conjugate'
 ):
@@ -280,6 +280,10 @@ def estimation_location_covariance_texture_RGD(
     Xopt, log = solver.solve(problem, x=init)
     Xopt[0] = Xopt[0].reshape((-1, 1))
 
+    # TODO: handle real data
+    # if X.dtype == np.float:
+    #    Xopt[0] = Xopt[0].real.astype(np.float64)
+    #    Xopt[2] = Xopt[2].real.astype(np.float64)
     return Xopt[0], Xopt[1], Xopt[2], log
 
 

tests/tests_features/test_location_covariance_texture.py
@@ -4,6 +4,7 @@ import autograd.numpy.linalg as la
 from autograd.numpy import random
 import numpy.testing as np_test
 
+from pyCovariance.features import location_covariance_texture
 from pyCovariance.features.location_covariance_texture import\
         create_cost_egrad_location_covariance_texture
 from pyCovariance.generation_data import\
@@ -31,7 +32,7 @@ def test_cost_location_covariance_texture():
     # test cost function value
     mu = random.randn(p, 1) + 1j*random.randn(p, 1)
     tau = generate_textures(N)
-    sigma = generate_complex_covariance(p)
+    sigma = generate_complex_covariance(p, unit_det=True)
     X = sample_complex_compound_distribution(tau, sigma)
     X = X + mu
     cost, _ = create_cost_egrad_location_covariance_texture(X)
@@ -54,7 +55,7 @@ def test_egrad_location_covariance_texture():
     # test egrad
     mu = random.randn(p, 1) + 1j*random.randn(p, 1)
     tau = generate_textures(N)
-    sigma = generate_complex_covariance(p)
+    sigma = generate_complex_covariance(p, unit_det=True)
     X = sample_complex_compound_distribution(tau, sigma)
     X = X + mu
     cost, grad_close_form = create_cost_egrad_location_covariance_texture(
@@ -70,3 +71,23 @@ def test_egrad_location_covariance_texture():
     np_test.assert_allclose(gc[1], gn[1])
     # test grad sigma
     np_test.assert_allclose(gc[2], gn[2])
+
+
+def test_complex_location_covariance_texture():
+    N = int(1e2)
+    p = 5
+    feature = location_covariance_texture(N, p)
+
+    mu = random.randn(p, 1) + 1j*random.randn(p, 1)
+    tau = generate_textures(N)
+    sigma = generate_complex_covariance(p, unit_det=True)
+    np_test.assert_almost_equal(la.det(sigma), 1)
+    X = sample_complex_compound_distribution(tau, sigma)
+    X = X + mu
+    assert X.dtype == np.complex128
+
+    res = feature.estimation(X).export()
+    assert res[0].dtype == np.complex128
+    assert res[2].dtype == np.complex128
+    assert la.norm(mu - res[0])/la.norm(mu) < 0.01
+    # assert la.norm(sigma - res[2])/la.norm(sigma) < 0.01