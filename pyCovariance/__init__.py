452ab532010be790bc8c93b8aa114964cfaee9c4
Antoine Collas <contact@antoinecollas.fr>
2020-12-15 22:29:23 +0100

    fix imports

--- pyCovariance/__init__.py
+++ pyCovariance/__init__.py
@@ -1,3 +1,5 @@
+__all__ = ['K_means_datacube', 'K_means', 'pca_image']
+
 from .cluster_datacube import K_means_datacube
 from .clustering_functions import K_means
 from .pca import pca_image
--- pyCovariance/features/__init__.py
+++ pyCovariance/features/__init__.py
@@ -1,3 +1,15 @@
+__all__ = [
+    'Feature',
+    'covariance',
+    'covariance_euclidean',
+    'covariance_texture',
+    'location_covariance_texture',
+    'intensity_euclidean',
+    'mean_pixel_euclidean',
+    'pixel_euclidean',
+    'tau_UUH'
+]
+
 from .base import Feature
 from .covariance import covariance, covariance_euclidean
 from .covariance_texture import covariance_texture
