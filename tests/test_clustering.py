306e3041724a3c01a9c4cf34e810444ee9167595
Antoine Collas <contact@antoinecollas.fr>
2021-02-02 13:39:49 +0100

    WIP: K-means following sklearn api

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -1,5 +1,4 @@
 import autograd.numpy as np
-import autograd.numpy.linalg as la
 import autograd.numpy.random as rnd
 import numpy.testing as np_test
 import os
@@ -7,11 +6,10 @@ import os
 from pyCovariance import K_means
 
 from pyCovariance.clustering import \
-        compute_objective_function,\
-        compute_means_parallel,\
-        compute_pairwise_distances_parallel,\
+        _compute_objective_function,\
+        _compute_pairwise_distances,\
         _K_means,\
-        random_index_for_initialisation
+        _random_index_for_initialisation
 
 from pyCovariance.features import\
         center_euclidean,\
@@ -24,80 +22,7 @@ from pyCovariance.generation_data import \
         sample_normal_distribution
 
 
-def test_compute_pairwise_distances_parallel():
-    p = 5
-    N = int(1e2)
-    N_mean = int(1e1)
-
-    X = _FeatureArray((p, ))
-    X.append(np.random.randn(N, p))
-    mu = _FeatureArray((p, ))
-    mu.append(np.random.randn(N_mean, p))
-    pix = center_euclidean(p)
-
-    # single thread
-    d = compute_pairwise_distances_parallel(X,
-                                            mu,
-                                            pix.distance,
-                                            nb_threads=0)
-    assert d.dtype == np.float64
-    assert d.shape == (N, N_mean)
-    for i in range(N):
-        for k in range(N_mean):
-            assert d[i, k] == la.norm(X[i].export()-mu[k].export())
-
-    # multiple thread
-    d = compute_pairwise_distances_parallel(X,
-                                            mu,
-                                            pix.distance,
-                                            nb_threads=os.cpu_count())
-    assert d.dtype == np.float64
-    assert d.shape == (N, N_mean)
-    for i in range(N):
-        for k in range(N_mean):
-            assert d[i, k] == la.norm(X[i].export()-mu[k].export())
-
-
-def test_compute_means_parallel():
-    p = 5
-    N = int(1e2)
-    K = int(1e1)
-
-    X = _FeatureArray((p, ))
-    X.append(np.random.randn(N, p))
-    C = np.random.randint(K, size=N)
-    assert C.shape == (N, )
-    pix = center_euclidean(p)
-
-    # single thread
-    m = compute_means_parallel(X,
-                               C,
-                               pix.mean,
-                               nb_threads=1).export()
-    assert m.dtype == np.float64
-    assert m.shape == (K, p)
-    for k in range(K):
-        np_test.assert_almost_equal(m[k], np.mean(X[C == k].export(), axis=0))
-
-    # multiple threads
-    m = compute_means_parallel(X,
-                               C,
-                               pix.mean,
-                               nb_threads=os.cpu_count()).export()
-    assert m.dtype == np.float64
-    assert m.shape == (K, p)
-    for k in range(K):
-        np_test.assert_almost_equal(m[k], np.mean(X[C == k].export(), axis=0))
-
-
-def test_random_index_for_initialisation():
-    K = 10
-    N = 20
-    idx = random_index_for_initialisation(K, N)
-    assert len(np.unique(idx)) == len(idx)
-
-
-def test_compute_objective_function():
+def test__compute_objective_function():
     N = int(1e3)
     K = 10
     distances = rnd.rand(N, K)
@@ -106,8 +31,15 @@ def test_compute_objective_function():
         d = distances[i]
         k = np.argmin(d)
         res += d[k]**2
-    var_intr = compute_objective_function(distances)
+    var_intr = _compute_objective_function(distances)
     np_test.assert_almost_equal(res, var_intr)
+
+
+def test__random_index_for_initialisation():
+    K = 10
+    N = 20
+    idx = _random_index_for_initialisation(K, N)
+    assert len(np.unique(idx)) == len(idx)
 
 
 def test__K_means():
@@ -132,55 +64,55 @@ def test__K_means():
     # plt.scatter(X.export()[:, 0], X.export()[:, 1], c=y)
     # plt.show()
 
-    pix = center_euclidean(p)
+    pix = center_euclidean()(p, N)
 
     # single thread
-    y_predict = _K_means(
+    y_pred = _K_means(
         X,
-        K=2,
+        n_clusters=2,
         distance=pix.distance,
         mean_function=pix.mean,
         init=None,
-        nb_threads=1,
+        n_jobs=1,
         verbose=False
     )[0]
-    precision = np.sum(y == y_predict)/(2*N)
+    precision = np.sum(y == y_pred)/(2*N)
     if precision < 0.5:
-        y_predict = np.mod(y_predict+1, 2)
-    precision = np.sum(y == y_predict)/(2*N)
+        y_pred = np.mod(y_pred+1, 2)
+    precision = np.sum(y == y_pred)/(2*N)
     assert precision >= 0.95
 
     # single thread with init
     init = np.concatenate([np.zeros(N), np.ones(N)])
-    y_predict = _K_means(
+    y_pred = _K_means(
         X,
-        K=2,
+        n_clusters=2,
         distance=pix.distance,
         mean_function=pix.mean,
         init=init,
-        nb_threads=1,
+        n_jobs=1,
         verbose=False
     )[0]
-    precision = np.sum(y == y_predict)/(2*N)
+    precision = np.sum(y == y_pred)/(2*N)
     if precision < 0.5:
-        y_predict = np.mod(y_predict+1, 2)
-    precision = np.sum(y == y_predict)/(2*N)
+        y_pred = np.mod(y_pred+1, 2)
+    precision = np.sum(y == y_pred)/(2*N)
     assert precision >= 0.95
 
     # multiple threads
-    y_predict = _K_means(
+    y_pred = _K_means(
         X,
-        K=2,
+        n_clusters=2,
         distance=pix.distance,
         mean_function=pix.mean,
         init=None,
-        nb_threads=os.cpu_count(),
+        n_jobs=os.cpu_count(),
         verbose=False
     )[0]
-    precision = np.sum(y == y_predict)/(2*N)
+    precision = np.sum(y == y_pred)/(2*N)
     if precision < 0.5:
-        y_predict = np.mod(y_predict+1, 2)
-    precision = np.sum(y == y_predict)/(2*N)
+        y_pred = np.mod(y_pred+1, 2)
+    precision = np.sum(y == y_pred)/(2*N)
     assert precision >= 0.95
 
     # test final criteria and mu
@@ -196,15 +128,15 @@ def test__K_means():
     idx = np.random.permutation(np.arange(2*N))
     y = y[idx]
     X = X[idx]
-    y_predict, mu, _, _, criterion_values = _K_means(
+    y_pred, mu, _, _, criterion_values = _K_means(
         X,
-        K=2,
+        n_clusters=2,
         distance=pix.distance,
         mean_function=pix.mean,
         init=None,
-        nb_init=20,
-        nb_threads=1,
-        iter_max=1,
+        n_init=20,
+        n_jobs=1,
+        max_iter=1,
         verbose=False
     )
     best_criterion_value = np.inf
@@ -212,95 +144,110 @@ def test__K_means():
         for j in range(len(criterion_values[i])):
             if best_criterion_value > criterion_values[i][j]:
                 best_criterion_value = criterion_values[i][j]
-    d = compute_pairwise_distances_parallel(
+    d = _compute_pairwise_distances(
         X,
         mu,
         pix.distance,
-        nb_threads=1
+        n_jobs=1
     )
-    criterion_value = compute_objective_function(d)
+    criterion_value = _compute_objective_function(d)
     assert np.abs(best_criterion_value - criterion_value) < 1e-8
 
 
 def test_K_means():
-    batch_size = 100
+    n_samples = 100
     N = 200
     p = 3
 
-    X = np.zeros((batch_size, p, N))
+    X = np.zeros((n_samples, p, N))
 
     # generating points of class 1
     cov1 = generate_covariance(p)
-    for i in range(batch_size//2):
+    for i in range(n_samples//2):
         X[i] = sample_normal_distribution(N, cov1)
 
     # generating points of class 2
     cov2 = generate_covariance(p)
-    for i in range(batch_size//2, batch_size):
+    for i in range(n_samples//2, n_samples):
         X[i] = sample_normal_distribution(N, cov2)
 
     # labels
     y = np.concatenate([
-        np.zeros(batch_size//2),
-        np.ones(batch_size - batch_size//2)
+        np.zeros(n_samples//2),
+        np.ones(n_samples - n_samples//2)
     ]).astype(int)
 
     assert X.shape[0] == len(y)
 
     # generating data points to cluster
-    idx = np.random.permutation(batch_size)
+    idx = np.random.permutation(n_samples)
     X = X[idx]
     y = y[idx]
 
-    feature = covariance(p)
+    feature = covariance()
 
     # single thread
-    y_predict = K_means(
-        X,
-        K=2,
-        feature=feature,
-        init=None,
-        nb_init=10,
-        iter_max=100,
-        nb_threads=1,
+    model = K_means(
+        feature,
+        n_clusters=2,
+        max_iter=100,
+        n_init=10,
+        n_jobs=1,
+        tol=1e-4,
         verbose=False
-    )[0]
-    precision = np.sum(y == y_predict)/batch_size
+    )
+    # fit
+    model.fit(X)
+    # predict
+    y_pred = model.predict(X)
+    precision = np.sum(y == y_pred)/n_samples
     if precision < 0.5:
-        y_predict = np.mod(y_predict+1, 2)
-    precision = np.sum(y == y_predict)/batch_size
-    assert precision >= 0.8
-
-    # single thread with init
-    init = np.concatenate([
-        np.zeros(batch_size//2),
-        np.ones(batch_size - batch_size//2)
-    ]).astype(int)
-    y_predict = K_means(
-        X,
-        K=2,
-        feature=feature,
-        init=init,
-        nb_threads=1,
-        verbose=False
-    )[0]
-    precision = np.sum(y == y_predict)/batch_size
+        y_pred = np.mod(y_pred+1, 2)
+    precision = np.sum(y == y_pred)/n_samples
+    assert precision >= 0.9
+    # transform
+    y_pred = model.predict(X)
+    d = model.transform(X)
+    assert d.shape == (len(X), 2)
+    y_pred_2 = d.argmin(axis=1)
+    assert (y_pred_2 == y_pred).all()
+    # predict_proba
+    proba = model.predict_proba(X)
+    assert proba.shape == (len(X), 2)
+    assert (proba >= 0).all()
+    actual = np.sum(proba, axis=1)
+    desired = np.ones(len(X))
+    assert ((actual - desired) < 1e-8).all()
+    actual = proba.argmax(axis=1)
+    assert (actual == y_pred).all()
+    # fit_predict
+    y_pred = model.fit_predict(X)
+    precision = np.sum(y == y_pred)/n_samples
     if precision < 0.5:
-        y_predict = np.mod(y_predict+1, 2)
-    precision = np.sum(y == y_predict)/batch_size
-    assert precision >= 0.8
+        y_pred = np.mod(y_pred+1, 2)
+    precision = np.sum(y == y_pred)/n_samples
+    assert precision >= 0.9
 
     # multiple threads
-    y_predict = K_means(
-        X,
-        K=2,
-        feature=feature,
-        init=None,
-        nb_threads=os.cpu_count(),
+    model = K_means(
+        feature,
+        n_clusters=2,
+        max_iter=100,
+        n_init=10,
+        n_jobs=-1,
+        tol=1e-4,
         verbose=False
-    )[0]
-    precision = np.sum(y == y_predict)/batch_size
+    )
+    model.fit(X)
+    y_pred = model.predict(X)
+    precision = np.sum(y == y_pred)/n_samples
     if precision < 0.5:
-        y_predict = np.mod(y_predict+1, 2)
-    precision = np.sum(y == y_predict)/batch_size
-    assert precision >= 0.8
+        y_pred = np.mod(y_pred+1, 2)
+    precision = np.sum(y == y_pred)/n_samples
+    assert precision >= 0.9
+    y_pred = model.fit_predict(X)
+    precision = np.sum(y == y_pred)/n_samples
+    if precision < 0.5:
+        y_pred = np.mod(y_pred+1, 2)
+    precision = np.sum(y == y_pred)/n_samples
+    assert precision >= 0.9
