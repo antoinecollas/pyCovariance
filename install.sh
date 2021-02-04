b0f915e27524219c6e0a216ff750c6829179cd88
Antoine Collas <contact@antoinecollas.fr>
2021-02-04 16:40:55 +0100

    simplify installation

--- install.sh
+++ install.sh
@@ -6,17 +6,5 @@ conda activate pyCovariance
 # install libraries
 pip install -r requirements.txt
 
-# install custom pymanopt
-DIR="../pymanopt"
-if [ ! -d "$DIR" ]
-then
-	cd ..
-	git clone https://github.com/antoinecollas/pymanopt
-	cd -
-fi
-cd ../pymanopt
-python setup.py install
-
 # install pyCovariance
-cd -
 python setup.py install
