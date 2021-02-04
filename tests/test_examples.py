8c450e319b6b453aa784c9f419ee094e51724c49
Antoine Collas <contact@antoinecollas.fr>
2021-02-04 18:44:56 +0100

    comment tests too heavy for github actions

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -49,55 +49,56 @@ def test_hyperspectral_indian_pines():
         verbose=False
     )
 
-
-def test_hyperspectral_pavia():
-    dataset_name = 'Pavia'
-    dataset = Dataset(dataset_name)
-
-    pairs_w_k = [(3, 4)]
-
-    features_list = list()
-    for w, k in pairs_w_k:
-        features_list.append([
-            'sklearn',
-        ])
-
-    k_means.main(
-        dataset=dataset,
-        crop_image=True,
-        nb_threads=1,
-        pairs_window_size_nb_bands=pairs_w_k,
-        mask=True,
-        features_list=features_list,
-        nb_init=1,
-        nb_iter_max=1,
-        verbose=False
-    )
-
-
-def test_hyperspectral_salinas():
-    dataset_name = 'Salinas'
-    dataset = Dataset(dataset_name)
-
-    pairs_w_k = [(3, 4)]
-
-    features_list = list()
-    for w, k in pairs_w_k:
-        features_list.append([
-            'sklearn',
-        ])
-
-    k_means.main(
-        dataset=dataset,
-        crop_image=True,
-        nb_threads=1,
-        pairs_window_size_nb_bands=pairs_w_k,
-        mask=True,
-        features_list=features_list,
-        nb_init=1,
-        nb_iter_max=1,
-        verbose=False
-    )
+# Pavia and Salilas are commented because data are too heavy
+# to run on github actions
+# def test_hyperspectral_pavia():
+#     dataset_name = 'Pavia'
+#     dataset = Dataset(dataset_name)
+#
+#     pairs_w_k = [(3, 4)]
+#
+#     features_list = list()
+#     for w, k in pairs_w_k:
+#         features_list.append([
+#             'sklearn',
+#         ])
+#
+#     k_means.main(
+#         dataset=dataset,
+#         crop_image=True,
+#         nb_threads=1,
+#         pairs_window_size_nb_bands=pairs_w_k,
+#         mask=True,
+#         features_list=features_list,
+#         nb_init=1,
+#         nb_iter_max=1,
+#         verbose=False
+#     )
+#
+#
+# def test_hyperspectral_salinas():
+#     dataset_name = 'Salinas'
+#     dataset = Dataset(dataset_name)
+#
+#     pairs_w_k = [(3, 4)]
+#
+#     features_list = list()
+#     for w, k in pairs_w_k:
+#         features_list.append([
+#             'sklearn',
+#         ])
+#
+#     k_means.main(
+#         dataset=dataset,
+#         crop_image=True,
+#         nb_threads=1,
+#         pairs_window_size_nb_bands=pairs_w_k,
+#         mask=True,
+#         features_list=features_list,
+#         nb_init=1,
+#         nb_iter_max=1,
+#         verbose=False
+#     )
 
 
 def test_plot_subspace():
