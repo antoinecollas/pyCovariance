68ad8527d82f329a340f3760813a072c09dfd614
Antoine Collas <contact@antoinecollas.fr>
2021-02-11 21:43:10 +0100

    add test of real-valued covariance+texture

--- tests/tests_features/test_covariance_texture.py
+++ tests/tests_features/test_covariance_texture.py
@@ -6,9 +6,87 @@ from pyCovariance.features import covariance, covariance_texture
 from pyCovariance.features.base import _FeatureArray
 from pyCovariance.generation_data import\
         generate_complex_covariance,\
+        generate_covariance,\
         generate_textures,\
-        sample_complex_compound_distribution
+        sample_complex_compound_distribution,\
+        sample_compound_distribution
 from pyCovariance.matrix_operators import invsqrtm
+
+
+def test_real_covariance_texture():
+    p = 5
+    N = int(2*1e5)
+    feature = covariance_texture()(p, N)
+    assert type(str(feature)) is str
+
+    # test estimation
+    tau = generate_textures(N)
+    sigma = generate_covariance(p, unit_det=True)
+    sigma = sigma/(la.det(sigma)**(1/p))
+    X = sample_compound_distribution(tau, sigma)
+
+    res = feature.estimation(X).export()
+    assert res[0].dtype == np.float64
+    assert res[0].shape == (p, p)
+    assert res[1].dtype == np.float64
+    assert res[1].shape == (N, 1)
+    assert la.norm(res[0]-sigma)/la.norm(sigma) < 0.01
+    np_test.assert_almost_equal(np.abs(la.det(res[0])), 1)
+
+    # test distance
+    data = _FeatureArray((p, p), (N, 1))
+    data.append([generate_covariance(p, unit_det=True),
+                 generate_textures(N)])
+    data.append([generate_covariance(p, unit_det=True),
+                 generate_textures(N)])
+
+    sigma0_isqrtm = invsqrtm(data[0].export()[0])
+    prod = sigma0_isqrtm@data[1].export()[0]@sigma0_isqrtm
+    eigvals = la.eigvalsh(prod)
+    d = (1/p)*np.sum(np.log(eigvals)**2)
+    temp = np.log(data[0].export()[1]) - np.log(data[1].export()[1])
+    d += (1/N) * la.norm(temp)**2
+    d = np.sqrt(d)
+    np_test.assert_almost_equal(feature.distance(data[0], data[1]), d)
+
+    # test distances
+    data = _FeatureArray((p, p), (N, 1))
+    data.append([generate_covariance(p, unit_det=True),
+                 generate_textures(N)])
+    data.append([generate_covariance(p, unit_det=True),
+                 generate_textures(N)])
+
+    sigma0_isqrtm = invsqrtm(data[0].export()[0])
+    prod = sigma0_isqrtm@data[1].export()[0]@sigma0_isqrtm
+    eigvals = la.eigvalsh(prod)
+    d_sigma = np.sqrt(np.sum(np.log(eigvals)**2))
+
+    temp = np.log(data[0].export()[1]) - np.log(data[1].export()[1])
+    d_tau = la.norm(temp)
+
+    d_M = np.sqrt((1/p) * d_sigma**2 + (1/N) * d_tau**2)
+    d = [d_M, d_sigma, d_tau]
+
+    np_test.assert_almost_equal(feature.distances(data[0], data[1]), d)
+
+    # test mean
+    N = 5
+    N_mean = 10
+    data = _FeatureArray((p, p), (N, 1))
+    for _ in range(N_mean):
+        data.append([generate_covariance(p, unit_det=True),
+                     generate_textures(N)])
+
+    cov = covariance()(p, N)
+    sigma = _FeatureArray((p, p))
+    sigma.append(data.export()[0])
+    mean_sigma = cov.mean(sigma).export()
+    mean_text = np.prod(data.export()[1], axis=0)**(1/N_mean)
+    m = feature.mean(data).export()
+    np_test.assert_almost_equal(la.det(m[0]), 1)
+    assert la.norm(m[0] - mean_sigma) / la.norm(mean_sigma) < 1e-6
+    assert la.norm(m[0] - mean_sigma) / la.norm(mean_sigma) < 1e-6
+    assert la.norm(m[1] - mean_text) / la.norm(mean_text) < 1e-6
 
 
 def test_complex_covariance_texture():
