bc0b971ed49bf43226f019ef415c1a669755988f
Antoine Collas <contact@antoinecollas.fr>
2021-02-12 17:57:26 +0100

    add test log of covariance + texture

--- tests/tests_features/test_covariance_texture.py
+++ tests/tests_features/test_covariance_texture.py
@@ -10,7 +10,7 @@ from pyCovariance.generation_data import\
         generate_textures,\
         sample_complex_compound_distribution,\
         sample_compound_distribution
-from pyCovariance.matrix_operators import invsqrtm
+from pyCovariance.matrix_operators import invsqrtm, logm, sqrtm
 
 
 def test_real_covariance_texture():
@@ -68,6 +68,92 @@ def test_real_covariance_texture():
     d = [d_M, d_sigma, d_tau]
 
     np_test.assert_almost_equal(feature.distances(data[0], data[1]), d)
+
+    # test log
+    X = _FeatureArray((p, p), (N, 1))
+    X.append([generate_covariance(p, unit_det=True),
+              generate_textures(N)])
+    Y = _FeatureArray((p, p), (N, 1))
+    Y.append([generate_covariance(p, unit_det=True),
+              generate_textures(N)])
+
+    log = feature.log(X, Y)
+    assert type(log) is _FeatureArray
+    assert len(log) == 1
+    log = log.export()
+    assert type(log) is list
+    assert len(log) == 2
+    assert log[0].dtype == np.float64
+    assert log[0].shape == (p, p)
+    assert log[1].dtype == np.float64
+    assert log[1].shape == (N, 1)
+    X_sqrtm = sqrtm(X.export()[0])
+    X_isqrtm = invsqrtm(X.export()[0])
+    temp = X_isqrtm @ Y.export()[0] @ X_isqrtm
+    temp = logm(temp)
+    desired_log = [X_sqrtm @ temp @ X_sqrtm]
+    temp = X.export()[1] * np.log(1/X.export()[1] * Y.export()[1])
+    desired_log.append(temp)
+    condition = la.norm(log[0]-desired_log[0])
+    condition = condition / la.norm(desired_log[0])
+    assert condition < 1e-3
+    condition = la.norm(log[1]-desired_log[1])
+    condition = condition / la.norm(desired_log[1])
+    assert condition < 1e-3
+
+    # test vectorized log
+    log = feature.log(X, Y, vectorize=True)
+    assert type(log) is np.ndarray
+    assert log.dtype == np.float64
+    assert log.shape == (1, p*(p+1)/2+N)
+    desired_log[0] = desired_log[0][np.triu_indices(p)][np.newaxis, ...]
+    desired_log[1] = desired_log[1].reshape((1, -1))
+    desired_log = np.concatenate(desired_log, axis=1)
+    np_test.assert_almost_equal(log, desired_log)
+
+    # test batch log and vectorized log
+    batch_size = 10
+    X = _FeatureArray((p, p), (N, 1))
+    X.append([generate_covariance(p, unit_det=True),
+              generate_textures(N)])
+    Y = _FeatureArray((p, p), (N, 1))
+    for _ in range(batch_size):
+        Y.append([generate_covariance(p, unit_det=True),
+                  generate_textures(N)])
+
+    log = feature.log(X, Y)
+    assert type(log) is _FeatureArray
+    assert len(log) == batch_size
+    log = log.export()
+    assert type(log) is list
+    assert len(log) == 2
+    assert log[0].dtype == np.float64
+    assert log[0].shape == (batch_size, p, p)
+    assert log[1].dtype == np.float64
+    assert log[1].shape == (batch_size, N, 1)
+    log_vec = feature.log(X, Y, vectorize=True)
+    assert type(log_vec) is np.ndarray
+    assert log_vec.dtype == np.float64
+    assert log_vec.shape == (batch_size, p*(p+1)/2+N)
+    for i in range(batch_size):
+        X_sqrtm = sqrtm(X.export()[0])
+        X_isqrtm = invsqrtm(X.export()[0])
+        temp = X_isqrtm @ Y.export()[0][i] @ X_isqrtm
+        temp = logm(temp)
+        desired_log = [X_sqrtm @ temp @ X_sqrtm]
+        temp = X.export()[1] * np.log(1/X.export()[1]*Y.export()[1][i])
+        desired_log.append(temp)
+        condition = la.norm(log[0][i]-desired_log[0])
+        condition = condition / la.norm(desired_log[0])
+        assert condition < 1e-3
+        condition = la.norm(log[1][i]-desired_log[1])
+        condition = condition / la.norm(desired_log[1])
+        assert condition < 1e-3
+
+        desired_log[0] = desired_log[0][np.triu_indices(p)][np.newaxis, ...]
+        desired_log[1] = desired_log[1].reshape((1, -1))
+        desired_log = np.concatenate(desired_log, axis=1)
+        np_test.assert_almost_equal(log_vec[i], desired_log.reshape(-1))
 
     # test mean
     N = 5
