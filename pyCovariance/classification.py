71e08c32f1d30d8f0fb80e882056456616df57e4
Antoine Collas <contact@antoinecollas.fr>
2021-02-16 16:00:51 +0100

    add classification in tangent space

--- pyCovariance/classification.py
+++ pyCovariance/classification.py
@@ -1,6 +1,7 @@
 import autograd.numpy as np
 from joblib import Parallel, delayed
 from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
+from sklearn.linear_model import LogisticRegression
 from sklearn.utils.extmath import softmax
 from time import time
 
@@ -57,6 +58,7 @@ def _compute_pairwise_distances(X, means, distance_fct, n_jobs=1):
 
 
 class MDM(BaseEstimator, ClassifierMixin, TransformerMixin):
+
     """Classification by Minimum Distance to Mean
     using covariance estimation and Riemannian geometry.
 
@@ -90,7 +92,7 @@ class MDM(BaseEstimator, ClassifierMixin, TransformerMixin):
         Parameters
         ----------
         X : ndarray, shape (n_samples, p, N)
-        y : ndarray shape (n_samples, 1)
+        y : ndarray shape (n_samples,)
 
         Returns
         -------
@@ -130,7 +132,7 @@ class MDM(BaseEstimator, ClassifierMixin, TransformerMixin):
 
         Returns
         -------
-        pred : ndarray of int, shape (n_samples, 1)
+        pred : ndarray of int, shape (n_samples,)
             the prediction for each sample according to the closest centroid.
         """
         distances = self.transform(X)
@@ -179,3 +181,145 @@ class MDM(BaseEstimator, ClassifierMixin, TransformerMixin):
             the softmax probabilities for each class.
         """
         return softmax(-self.transform(X))
+
+
+class TSclassifier(BaseEstimator, ClassifierMixin):
+
+    """Classification in the tangent space.
+
+    Parameters
+    ----------
+    feature : a Feature from pyCovariance.features
+        e.g see pyCovariance/features/covariance.py
+    clf: sklearn classifier (default LogisticRegression)
+        The classifier to apply in the tangent space.
+    n_jobs : int, (default: 1)
+        The number of jobs to use for the computation. This works by computing
+        each of the class centroid in parallel.
+        If -1 all CPUs are used. If 1 is given, no parallel computing code is
+        used at all, which is useful for debugging. For n_jobs below -1,
+        (n_cpus + 1 + n_jobs) are used. Thus for n_jobs = -2, all CPUs but one
+        are used.
+    verbose: bool
+    """
+
+    def __init__(
+        self,
+        feature,
+        clf=LogisticRegression(max_iter=int(1e3)),
+        n_jobs=1,
+        verbose=False
+    ):
+        """Init."""
+        self.base_feature = feature
+        self.clf = clf
+        self.n_jobs = n_jobs
+        self.verbose = verbose
+
+        if not isinstance(clf, ClassifierMixin):
+            raise TypeError('clf must be a ClassifierMixin')
+
+    def fit(self, X, y):
+        """Fit TSclassifier.
+        Parameters
+        ----------
+        X : ndarray, shape (n_samples, p, N)
+        y : ndarray shape (n_samples,)
+
+        Returns
+        -------
+        self : TSclassifier instance.
+        """
+        verbose = self.verbose
+        n_jobs = self.n_jobs
+
+        n_samples, p, N = X.shape
+        feature = self.feature = self.base_feature(p, N)
+
+        clf = self.clf
+
+        if verbose:
+            print('Feature: ' + str(feature))
+            print('TSclassifier fitting ...')
+
+        t_beginning = time()
+
+        # features estimation
+        X = _estimate_features(X, feature.estimation, n_jobs)
+
+        # centroid computation
+        mean = self._mean = _compute_means(
+            X,
+            y=np.zeros((n_samples)),
+            mean_fct=feature.mean,
+            n_jobs=n_jobs
+        )
+
+        # log of X at self._mean
+        X = feature.log(mean, X, vectorize=True)
+
+        # classifier fitting
+        self.clf = clf.fit(X, y)
+
+        if verbose:
+            print('TSclassifier fitting done in %f s.'
+                  % (time() - t_beginning))
+
+        return self
+
+    def predict(self, X):
+        """get the predictions.
+        Parameters
+        ----------
+        X : ndarray, shape (n_samples, p, N)
+
+        Returns
+        -------
+        pred : ndarray of int, shape (n_trials,)
+        """
+        feature = self.feature
+        mean = self._mean
+        n_jobs = self.n_jobs
+        clf = self.clf
+
+        # features estimation
+        X = _estimate_features(X, feature.estimation, n_jobs)
+
+        # log of X at self._mean
+        X = feature.log(mean, X, vectorize=True)
+
+        # classifier prediction
+        pred = clf.predict(X)
+
+        return pred
+
+    def fit_predict(self, X, y):
+        """Fit and predict in one function."""
+        self.fit(X, y)
+        return self.predict(X)
+
+    def predict_proba(self, X):
+        """get the probabilities.
+        Parameters
+        ----------
+        X : ndarray, shape (n_samples, p, N)
+
+        Returns
+        -------
+        pred : ndarray of int, shape (n_trials, 1)
+        """
+        feature = self.feature
+        mean = self._mean
+        n_jobs = self.n_jobs
+        clf = self.clf
+
+        # features estimation
+        X = _estimate_features(X, feature.estimation, n_jobs)
+
+        # log of X at self._mean
+        X = feature.log(mean, X, vectorize=True)
+
+        # classifier prediction
+        pred = clf.predict_proba(X)
+
+        return pred
--- tests/test_classification.py
+++ tests/test_classification.py
@@ -10,7 +10,8 @@ from pyCovariance.classification import\
         _estimate_features,\
         _compute_means,\
         _compute_pairwise_distances,\
-        MDM
+        MDM,\
+        TSclassifier
 from pyCovariance.features import\
         center_euclidean,\
         mean_vector_euclidean
@@ -112,7 +113,7 @@ def test__compute_pairwise_distances():
             assert d[i, k] == la.norm(X[i].export()-mu[k].export())
 
 
-def helper_test_classification(clf):
+def helper_test_MDM(clf):
     n_classes = 5
     size_class = 100
     n_datasets = 3
@@ -128,7 +129,7 @@ def helper_test_classification(clf):
         y = list()
         for k in range(n_classes):
             cov = generate_covariance(p)
-            mu = 10 * rnd.randn(p, 1)
+            mu = rnd.randn(p, 1)
             for i in range(size_class):
                 X[i + k*size_class] = sample_normal_distribution(N, cov) + mu
 
@@ -183,10 +184,83 @@ def helper_test_classification(clf):
 def test_MDM_single_job():
     feature = mean_vector_euclidean()
     mdm = MDM(feature, n_jobs=1)
-    helper_test_classification(mdm)
+    helper_test_MDM(mdm)
 
 
 def test_MDM_multiple_jobs():
     feature = mean_vector_euclidean()
     mdm = MDM(feature, n_jobs=-1)
-    helper_test_classification(mdm)
+    helper_test_MDM(mdm)
+
+
+def helper_test_classifier(clf):
+    n_classes = 5
+    size_class = 100
+    n_datasets = 3
+    n_samples = size_class * n_classes
+
+    for j in range(n_datasets):
+        # test several p, N
+        N = 20 + 5*j
+        p = 3 + 2*j
+
+        X = np.zeros((n_samples, p, N))
+
+        y = list()
+        for k in range(n_classes):
+            cov = generate_covariance(p)
+            mu = rnd.randn(p, 1)
+            for i in range(size_class):
+                X[i + k*size_class] = sample_normal_distribution(N, cov) + mu
+
+            y.append(k*np.ones(size_class))
+        y = np.concatenate(y)
+        assert X.shape[0] == len(y)
+
+        idx = rnd.permutation(n_samples)
+
+        # generating training data
+        X_train = X[idx[:n_samples//2]]
+        y_train = y[idx[:n_samples//2]]
+
+        # generating test data
+        X_test = X[idx[n_samples//2:]]
+        y_test = y[idx[n_samples//2:]]
+
+        # fit + predict on train set
+        clf.fit(X_train, y_train)
+        mean = clf._mean
+        temp = np.mean(X_train, axis=2)
+        temp = np.mean(temp, axis=0)
+        np_test.assert_almost_equal(mean.export(), temp)
+        y = clf.predict(X_train)
+        assert accuracy_score(y_train, y) > 0.95
+
+        # predict on test set
+        y = clf.predict(X_test)
+        assert accuracy_score(y_test, y) > 0.95
+
+        # predict proba
+        proba = clf.predict_proba(X_test)
+        assert (proba >= 0).all()
+        actual = np.sum(proba, axis=1)
+        desired = np.ones(len(X_test))
+        assert ((actual - desired) < 1e-8).all()
+        actual = proba.argmax(axis=1)
+        assert (actual == y).all()
+
+        # fit_predict
+        y = clf.fit_predict(X_train, y_train)
+        assert accuracy_score(y_train, y) > 0.95
+
+
+def test_TSclassifier_single_job():
+    feature = mean_vector_euclidean()
+    clf = TSclassifier(feature, n_jobs=1)
+    helper_test_classifier(clf)
+
+
+def test_TSclassifier_multiple_job():
+    feature = mean_vector_euclidean()
+    clf = TSclassifier(feature, n_jobs=-1)
+    helper_test_classifier(clf)
