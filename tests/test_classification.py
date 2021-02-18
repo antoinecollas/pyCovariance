78de3bb963684c571aeb1b60254462b455ff23b3
Antoine Collas <contact@antoinecollas.fr>
2021-02-18 11:25:27 +0100

    change TSclassifier to TangentSpace
    classifier --> transformer

--- tests/test_classification.py
+++ tests/test_classification.py
@@ -4,10 +4,9 @@ import numpy.testing as np_test
 from sklearn.metrics import accuracy_score
 
 from pyCovariance.classification import\
-        MDM,\
-        TSclassifier
+        MDM
 from pyCovariance.features import\
-        mean_vector_euclidean
+        center_euclidean
 from pyCovariance.generation_data import\
         generate_covariance,\
         sample_normal_distribution
@@ -21,7 +20,7 @@ def helper_test_MDM(clf):
 
     for j in range(n_datasets):
         # test several p, N
-        N = 20 + 5*j
+        N = 21 + 5*j
         p = 3 + j
 
         X = np.zeros((n_samples, p, N))
@@ -29,7 +28,7 @@ def helper_test_MDM(clf):
         y = list()
         for k in range(n_classes):
             cov = generate_covariance(p)
-            mu = rnd.randn(p, 1)
+            mu = 5*rnd.randn(p, 1)
             for i in range(size_class):
                 X[i + k*size_class] = sample_normal_distribution(N, cov) + mu
 
@@ -52,7 +51,7 @@ def helper_test_MDM(clf):
         means = clf._means
         for i, mean in enumerate(means):
             temp = X_train[y_train == i]
-            temp = np.mean(temp, axis=2)
+            temp = temp[:, :, N//2]
             temp = np.mean(temp, axis=0)
             np_test.assert_almost_equal(mean.export(), temp)
         y = clf.predict(X_train)
@@ -84,7 +83,7 @@ def helper_test_MDM(clf):
 def test_MDM_single_job():
     rnd.seed(123)
 
-    feature = mean_vector_euclidean()
+    feature = center_euclidean()
     mdm = MDM(feature, n_jobs=1)
     helper_test_MDM(mdm)
 
@@ -92,83 +91,6 @@ def test_MDM_single_job():
 def test_MDM_multiple_jobs():
     rnd.seed(123)
 
-    feature = mean_vector_euclidean()
+    feature = center_euclidean()
     mdm = MDM(feature, n_jobs=-1)
     helper_test_MDM(mdm)
-
-
-def helper_test_classifier(clf):
-    n_classes = 5
-    size_class = 100
-    n_datasets = 3
-    n_samples = size_class * n_classes
-
-    for j in range(n_datasets):
-        # test several p, N
-        N = 20 + 5*j
-        p = 3 + 2*j
-
-        X = np.zeros((n_samples, p, N))
-
-        y = list()
-        for k in range(n_classes):
-            cov = generate_covariance(p)
-            mu = rnd.randn(p, 1)
-            for i in range(size_class):
-                X[i + k*size_class] = sample_normal_distribution(N, cov) + mu
-
-            y.append(k*np.ones(size_class))
-        y = np.concatenate(y)
-        assert X.shape[0] == len(y)
-
-        idx = rnd.permutation(n_samples)
-
-        # generating training data
-        X_train = X[idx[:n_samples//2]]
-        y_train = y[idx[:n_samples//2]]
-
-        # generating test data
-        X_test = X[idx[n_samples//2:]]
-        y_test = y[idx[n_samples//2:]]
-
-        # fit + predict on train set
-        clf.fit(X_train, y_train)
-        mean = clf._mean
-        temp = np.mean(X_train, axis=2)
-        temp = np.mean(temp, axis=0)
-        np_test.assert_almost_equal(mean.export(), temp)
-        y = clf.predict(X_train)
-        assert accuracy_score(y_train, y) > 0.95
-
-        # predict on test set
-        y = clf.predict(X_test)
-        assert accuracy_score(y_test, y) > 0.95
-
-        # predict proba
-        proba = clf.predict_proba(X_test)
-        assert (proba >= 0).all()
-        actual = np.sum(proba, axis=1)
-        desired = np.ones(len(X_test))
-        assert ((actual - desired) < 1e-8).all()
-        actual = proba.argmax(axis=1)
-        assert (actual == y).all()
-
-        # fit_predict
-        y = clf.fit_predict(X_train, y_train)
-        assert accuracy_score(y_train, y) > 0.95
-
-
-def test_TSclassifier_single_job():
-    rnd.seed(123)
-
-    feature = mean_vector_euclidean()
-    clf = TSclassifier(feature, n_jobs=1)
-    helper_test_classifier(clf)
-
-
-def test_TSclassifier_multiple_job():
-    rnd.seed(123)
-
-    feature = mean_vector_euclidean()
-    clf = TSclassifier(feature, n_jobs=-1)
-    helper_test_classifier(clf)
---
+++ tests/test_tangent_space.py
@@ -0,0 +1,76 @@
+import autograd.numpy as np
+import autograd.numpy.linalg as la
+import autograd.numpy.random as rnd
+import numpy.testing as np_test
+
+from pyCovariance.tangent_space import\
+        TangentSpace
+from pyCovariance.features import\
+        center_euclidean
+from pyCovariance.generation_data import\
+        generate_covariance,\
+        sample_normal_distribution
+
+
+def helper_TangentSpace(ts):
+    n_samples = int(1e3)
+    p = 5
+    N = 45
+
+    X = np.zeros((n_samples, p, N))
+    cov = generate_covariance(p)
+    mu = rnd.randn(p, 1)
+    for i in range(n_samples):
+        X[i] = sample_normal_distribution(N, cov) + mu
+
+    # fit
+    ts.fit(X)
+    mean = ts._mean.export()
+    temp = X[:, :, N//2]
+    mean_desired = np.mean(temp, axis=0)
+    condition = la.norm(mean - mean_desired) / la.norm(mean_desired)
+    assert condition < 0.01
+
+    # transform
+    X_ts = ts.transform(X)
+    assert type(X_ts) is np.ndarray
+    assert X_ts.dtype == np.float64
+    assert X_ts.shape == (n_samples, p)
+    temp = X[:, :, N//2]
+    X_ts_desired = temp - mean_desired
+    np_test.assert_almost_equal(X_ts, X_ts_desired)
+
+    # fit_transform
+    X_ts = ts.fit_transform(X)
+    assert type(X_ts) is np.ndarray
+    assert X_ts.dtype == np.float64
+    assert X_ts.shape == (n_samples, p)
+    np_test.assert_almost_equal(X_ts, X_ts_desired)
+
+    # transform other data with same mean
+    mu = 10*rnd.randn(p, 1)
+    for i in range(n_samples):
+        X[i] = sample_normal_distribution(N, cov) + mu
+    X_ts = ts.transform(X)
+    assert type(X_ts) is np.ndarray
+    assert X_ts.dtype == np.float64
+    assert X_ts.shape == (n_samples, p)
+    temp = X[:, :, N//2]
+    X_ts_desired = temp - mean_desired
+    np_test.assert_almost_equal(X_ts, X_ts_desired)
+
+
+def test_TangentSpace_single_job():
+    rnd.seed(123)
+
+    feature = center_euclidean()
+    ts = TangentSpace(feature, n_jobs=1)
+    helper_TangentSpace(ts)
+
+
+def test_TangentSpace_multiple_job():
+    rnd.seed(123)
+
+    feature = center_euclidean()
+    ts = TangentSpace(feature, n_jobs=-1)
+    helper_TangentSpace(ts)
